a66fa490b0cdc7b78f3c51c09b1853c689d5cd1f
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-10-28 11:03:10 +0800

    Added modal listener

--- app/src/main/java/com/iics/ust/tigercup/Shops.java
+++ app/src/main/java/com/iics/ust/tigercup/Shops.java
@@ -1,18 +1,30 @@
 package com.iics.ust.tigercup;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class Shops extends AppCompatActivity {
+
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shops);
-    }
 
-    public void viewShopModal(View v){
-
+        button = findViewById(R.id.shop_1);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                final Dialog dialog = new Dialog(Shops.this);
+                dialog.setContentView(R.layout.activity_shop_modal);
+                dialog.show();
+            }
+        });
     }
 }
