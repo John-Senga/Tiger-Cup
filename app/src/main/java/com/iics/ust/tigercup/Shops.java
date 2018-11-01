d2f7cf915328ba733f9af0f00447e82195fb075f
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-11-01 10:04:04 +0800

    Programmatically add ImageButton

--- app/src/main/java/com/iics/ust/tigercup/Shops.java
+++ app/src/main/java/com/iics/ust/tigercup/Shops.java
@@ -7,24 +7,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class Shops extends AppCompatActivity {
 
     private ImageButton button;
+    private String[]Data = {
+        "iChill Theater Cafe", "Cafe Kivhan Coffee", "Figaro Coffee Company", "Beyond Coffee Manila",
+        "Amo Yamie Crib España", "Starbucks Coffee Dapitan Branch", "Café Churro", "Coffee Indulgence",
+        "Cafe-UK", "Starbucks P. Noval", "Floti Cafe", "Seattle's Best", "Cafe-UK Co. - Mendiola",
+        "Starbucks Coffee", "Cafe UK Dapitan", "Wit Avenue Cafe & Bar", "Bo's Coffee", "La Taza",
+        "Amor Bakery", "Mamang Taho Coffee Shop", "Bon AppeTEA - España", "Starbucks Coffee",
+        "Fritzo Cafe", "Hub Urban Coffee"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shops);
 
-        button = findViewById(R.id.shop_1);
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                final Dialog dialog = new Dialog(Shops.this);
-                dialog.setContentView(R.layout.activity_shop_modal);
-                dialog.show();
-            }
-        });
+        LinearLayout layout = findViewById(R.id.shopContainer);
+
+        for(int i = 0; i<Data.length; i++){
+            ImageButton shop = new ImageButton(this);
+
+            //Width and Height
+            shop.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 250));
+
+            //Image
+            shop.setImageResource(R.drawable.sample_2);
+
+            //Modal
+            shop.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    final Dialog dialog = new Dialog(Shops.this);
+                    dialog.setContentView(R.layout.activity_shop_modal);
+                    dialog.show();
+                }
+            });
+            
+            layout.addView(shop);
+        }
     }
 }
