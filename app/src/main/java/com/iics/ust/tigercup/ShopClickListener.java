cf9f24ec467b934a8aadb8d1b113e2cac9e61b34
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-11-18 20:52:33 +0800

    Added click lister for map button

--- app/src/main/java/com/iics/ust/tigercup/ShopClickListener.java
+++ app/src/main/java/com/iics/ust/tigercup/ShopClickListener.java
@@ -1,8 +1,12 @@
 package com.iics.ust.tigercup;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ShopClickListener implements View.OnClickListener {
@@ -27,6 +31,19 @@ public class ShopClickListener implements View.OnClickListener {
         //image
         ImageView img = dialog.findViewById(R.id.imageView);
         img.setImageResource(resourceId);
+
+        Button mapBtn = dialog.findViewById(R.id.map);
+        mapBtn.setOnClickListener(new View.OnClickListener()
+           {
+               @Override
+               public void onClick(View v) {
+                   Intent i = new Intent(Intent.ACTION_VIEW);
+                   i.setData(Uri.parse("geo:14.610378, 120.991916"));
+                   Intent chooser = Intent.createChooser(i,"Please select a map application");
+                   context.startActivity(chooser);
+               }
+           }
+        );
 
         dialog.show();
     }
