40e5591d94df028079337050c7e43ca3b565db17
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-11-25 10:57:46 +0800

    Added register screen

--- app/src/main/java/com/iics/ust/tigercup/Login.java
+++ app/src/main/java/com/iics/ust/tigercup/Login.java
@@ -1,7 +1,9 @@
 package com.iics.ust.tigercup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View v){
+        Intent i = null;
+        i = new Intent(this, Home.class);
+        startActivity(i);
+    }
+
+    public void goToRegister(View v){
+        Intent i = null;
+        i = new Intent(this, Register.class);
+        startActivity(i);
     }
 }
