fb672cf8d32fcd28e5fafdc4ac7b9ea2a822e0d4
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-11-25 12:20:34 +0800

    Added register function

--- app/src/main/java/com/iics/ust/tigercup/Register.java
+++ app/src/main/java/com/iics/ust/tigercup/Register.java
@@ -1,13 +1,57 @@
 package com.iics.ust.tigercup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
+    FirebaseDatabase db;
+    DatabaseReference root;
+    EditText fname, lname, email, password, confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        db = FirebaseDatabase.getInstance();
+        root = db.getReference("users");
+
+        fname = findViewById(R.id.fname);
+        lname = findViewById(R.id.lname);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirm = findViewById(R.id.confirm);
+    }
+
+    public void submit(View v){
+        String fname = this.fname.getText().toString().trim();
+        String lname = this.lname.getText().toString().trim();
+        String email = this.email.getText().toString().trim();
+        String password = this.password.getText().toString().trim();
+        String confirm = this.confirm.getText().toString().trim();
+
+        if(fname.equals("")||lname.equals("")||email.equals("")||password.equals("")||confirm.equals("")){
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+        }
+        else if(!password.equals(confirm)){
+            Toast.makeText(this, "Password does not match the confirm password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            UserData user = new UserData(fname, lname, email, password);
+            String key = root.push().getKey();
+            root.child(key).setValue(user);
+
+            Intent i = null;
+            i = new Intent(this, Home.class);
+            startActivity(i);
+        }
     }
 }
---
+++ app/src/main/java/com/iics/ust/tigercup/UserData.java
@@ -0,0 +1,12 @@
+package com.iics.ust.tigercup;
+
+public class UserData {
+    public String fname, lname, email, password;
+
+    public UserData(String fname, String lname, String email, String password) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.password = password;
+    }
+}
