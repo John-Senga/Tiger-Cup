2691b512126f110414dabb47c2c62ca9bac2c02d
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-11-25 14:01:51 +0800

    Added logout function

--- app/src/main/java/com/iics/ust/tigercup/Home.java
+++ app/src/main/java/com/iics/ust/tigercup/Home.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Home extends AppCompatActivity {
@@ -16,6 +19,27 @@ public class Home extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logout:
+                Intent i = null;
+                i = new Intent(this, Login.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     public void goToShops(View v){
