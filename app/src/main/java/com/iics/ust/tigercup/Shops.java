40edb9ebc919616d41e7d76b61f96bac7118de5c
John-Senga <2015081653@ust-ics.mygbiz.com>
2018-11-25 18:17:01 +0800

    Added toolbar on shop screen

--- app/src/main/java/com/iics/ust/tigercup/Shops.java
+++ app/src/main/java/com/iics/ust/tigercup/Shops.java
@@ -1,10 +1,16 @@
 package com.iics.ust.tigercup;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -24,6 +30,37 @@ public class Shops extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shops);
         getShops();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logout:
+                SharedPreferences pref = getApplicationContext().getSharedPreferences("data", MODE_PRIVATE);
+                SharedPreferences.Editor writer = pref.edit();
+                writer.remove("key");
+                writer.commit();
+
+                Intent i = null;
+                i = new Intent(this, Login.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     public void getShops(){
